ba4cbfe7d70c8815e8a2bfcd23af0f98f7a95dff
nOObDeViL <46815509+nOObDeViL@users.noreply.github.com>
2026-03-04 01:52:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
+from telethon import TelegramClient
 import os
 import asyncio
-import random
-from telethon import TelegramClient, events
-from telethon.errors import FloodWaitError
 
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 
-source_channel = "mavanisunny"
-earnkaro_bot = "EarnKaroBot"  # apna bot username
-
-client = TelegramClient("session.session", api_id, api_hash)
-
-@client.on(events.NewMessage(chats=source_channel))
-async def handler(event):
-    await asyncio.sleep(random.randint(5,15))
-    try:
-        await event.forward_to(earnkaro_bot)
-        print("Forwarded")
-    except FloodWaitError as e:
-        await asyncio.sleep(e.seconds)
-        await event.forward_to(earnkaro_bot)
+client = TelegramClient("session", api_id, api_hash)
 
 async def main():
+    await client.connect()
+
+    if not await client.is_user_authorized():
+        print("Session invalid. Generate new session locally.")
+        return
+
     print("Bot started successfully")
-    await client.start()
     await client.run_until_disconnected()
 
 asyncio.run(main())
